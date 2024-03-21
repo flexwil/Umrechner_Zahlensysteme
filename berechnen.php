7cdad7e824a3ca89415ffe7ff6576eb81dce29d3
flexwil <Felix.Wilson@outlook.de>
2024-03-21 19:14:21 +0100

    zurückführung in eine Datei

--- berechnen.php
+++ berechnen.php
@@ -1,11 +1,42 @@
+<html>
+<head><title>Umrechner</title></head>
+<body>
+<header> <h1>Dein Umrechner für Zahlensysteme</h1> </header>
+
+
+<form action="berechnen.php" method="POST" >
+    <label for="eingabe">Was möchtest du umrechnen?</label>
+    <input type="text" id="eingabe" name="eingabe">
+    <br>
+    <br>
+    <label for="EingabeSys">Zahl 2</label>
+    <select id="EingabeSys" name="EingabeSys">
+        <option value="bin">Binär</option>
+        <option value="dez">Dezimal</option>
+        <option value="hex">Hexadezimal</option>
+        <option value="okt">Oktal</option>
+    </select>
+    <br>
+    <br>
+    <label for="AusgabeSys">In Welches System soll umgewandelt werden?</label>
+    <input type="checkbox" id="nachbin">Binär<br>
+    <input type="checkbox" id="nachdez">Dezimal<br>
+    <input type="checkbox" id="nachhex">Hexadezimal<br>
+    <input type="checkbox" id="nachokt">Oktal<br>
+    <br>
+    <input value="Berechnen" type="submit">
+</form>
+</body>
+</html>
+
 <?php
-echo "Hello World" ."<br>";
-$alex = "Prima Typ hoch";
-$b = 5.8;
+echo "Das Ergebnis beträgt: ";
+$zahl1 = $_POST["eingabe"];    
+$zahl2 = $_POST["EingabeSys"];
 
-echo $alex.$b;
-echo $z1;
+$zzw = $zahl1/2;
+$zm = $zahl1%2;
 
-
+echo $zzw . "<br>";
+echo $zm;
 ?>
-
